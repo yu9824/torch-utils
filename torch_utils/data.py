7bbaa89702aed22d30ee2ea052d16b59d31b1694
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-11-11 16:57:17 +0900

    Update torch_utils/data.py to show progress bar

--- torch_utils/data.py
+++ torch_utils/data.py
@@ -1,29 +1,29 @@
 from collections.abc import Sequence
 from typing import SupportsIndex, Optional
 
+from numpy.typing import ArrayLike
 import rdkit.Chem.rdchem
 from joblib import Parallel, delayed
 import torch.utils.data
 import torch_geometric.data
 
 from torch_utils.feat import mol2data
+from torch_utils.utils import check_tqdm
 
 
 class GraphDataset(torch.utils.data.Dataset):
     def __init__(
         self,
         mols: Sequence[rdkit.Chem.rdchem.Mol],
-        y: Sequence[torch.Tensor],
+        y: Sequence[ArrayLike],
         n_jobs: Optional[int] = None,
+        ipynb: bool = False,
+        silent: bool = False,
     ) -> None:
         super().__init__()
         self.mols = mols if hasattr(mols, "__len__") else tuple(mols)
         self.n_jobs = n_jobs
-        self.y = (
-            torch.tensor(y, dtype=torch.float32)
-            if hasattr(y, "__len__")
-            else tuple(y)
-        )
+        self.y = y if hasattr(y, "__len__") else tuple(y)
 
         # check the length of mols and y
         assert len(self.mols) == len(self.y), (
@@ -37,12 +37,19 @@ class GraphDataset(torch.utils.data.Dataset):
         )(
             delayed(mol2data)(
                 mol=mol,
-                y=_y.view(1, -1),
+                y=torch.Tensor(_y).view(
+                    1, -1
+                ),  # To match shape in loss calculation
                 use_chirality=False,
                 use_partial_charge=False,
                 use_edges=False,
             )
-            for mol, _y in zip(self.mols, self.y)
+            for mol, _y in check_tqdm(
+                zip(self.mols, self.y),
+                total=len(self.mols),
+                ipynb=ipynb,
+                silent=silent,
+            )
         )
 
     def __len__(self) -> int:
