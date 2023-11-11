bcb0ffb099070df5606df0bb494bffad153ade3f
yu9824 <58211916+yu9824@users.noreply.github.com>
2023-11-11 15:06:33 +0900

    Create __init__.py

---
+++ torch_utils/__init__.py
@@ -0,0 +1,13 @@
+"""
+This is my own utility package for PyTorch.
+
+Copyright © 2021 yu9824
+"""
+
+__version__ = "0.0.1"
+__license__ = "MIT"
+__author__ = "yu9824"
+__copyright__ = "Copyright © 2023 yu9824"
+__author_email__ = "yu.9824.job@gmail.com"
+__user_id__ = "yu9824"
+__url__ = "https://github.com/yu9824/torch_utils"
