d1b41a234b12d4473065f4978c9df1ac5abd864f
yu9824 <58211916+yu9824@users.noreply.github.com>
2024-04-21 21:01:32 +0900

    Update data

--- torch_utils/data.py
+++ torch_utils/data.py
@@ -1,66 +1,74 @@
-from collections.abc import Sequence
-from typing import Optional, SupportsIndex
+import sys
 
-import rdkit.Chem.rdchem
+if sys.version_info >= (3, 9):
+    from collections.abc import Sequence
+else:
+    from typing import Sequence
+import os
+import pickle
+from dataclasses import dataclass
+from typing import SupportsIndex, Union
+
 import torch.utils.data
 import torch_geometric.data
-from joblib import Parallel, delayed
-from numpy.typing import ArrayLike
+from rdkit import Chem
 
 from torch_utils.feat import mol2data
 from torch_utils.utils import check_tqdm
 
 
+@dataclass
 class GraphDataset(torch.utils.data.Dataset):
-    def __init__(
+    _smiles_set: Sequence[str]
+    _y: Sequence[float]
+    dirpath_cache: Union[os.PathLike, str] = "./_cache"
+    use_chirality: bool = False
+    use_partial_charge: bool = False
+    use_edges: bool = False
+
+    def __post_init__(
         self,
-        mols: Sequence[rdkit.Chem.rdchem.Mol],
-        y: Sequence[ArrayLike],
-        use_chirality: bool = False,
-        use_partial_charge: bool = False,
-        use_edges: bool = False,
-        n_jobs: Optional[int] = None,
-        silent: bool = False,
     ) -> None:
-        super().__init__()
-        self.mols = mols if hasattr(mols, "__len__") else tuple(mols)
-        self.n_jobs = n_jobs
-        self.y = y if hasattr(y, "__len__") else tuple(y)
-
-        # check the length of mols and y
-        assert len(self.mols) == len(self.y), (
-            "The length of mols and y must be same."
-            f"{len(self.mols)} != {len(self.y)}"
+        _kwargs_mol2data = dict(
+            use_chirality=self.use_chirality,
+            use_partial_charge=self.use_partial_charge,
+            use_edges=self.use_edges,
         )
+        self._dataset: Sequence[torch_geometric.data.Data] = []
 
-        # convert rdkit.Chem.rdchem.Mol to torch_geometric.data.Data
-        self._data: list[torch_geometric.data.Data] = Parallel(
-            n_jobs=self.n_jobs
-        )(
-            delayed(mol2data)(
-                mol=mol,
-                y=torch.Tensor(_y).view(
-                    1, -1
-                ),  # To match shape in loss calculation
-                use_chirality=use_chirality,
-                use_partial_charge=use_partial_charge,
-                use_edges=use_edges,
+        for _idx_sample, _smiles in check_tqdm(
+            enumerate(self._smiles_set), total=len(self._smiles_set)
+        ):
+            _filepath_cache = os.path.join(
+                self.dirpath_cache,
+                str(_kwargs_mol2data),
+                f"{_idx_sample}.pkl",
             )
-            for mol, _y in check_tqdm(
-                zip(self.mols, self.y),
-                total=len(self.mols),
-                silent=silent,
-            )
-        )
+            if os.path.isfile(_filepath_cache):
+                with open(_filepath_cache, mode="rb") as f:
+                    _data: torch_geometric.data.Data = pickle.load(f)
+            else:
+                _data: torch_geometric.data.Data = mol2data(
+                    Chem.MolFromSmiles(_smiles), **_kwargs_mol2data
+                )
+                os.makedirs(os.path.dirname(_filepath_cache), exist_ok=True)
+                with open(_filepath_cache, mode="wb") as f:
+                    pickle.dump(_data, f)
+
+            _data.y = torch.tensor(self._y[_idx_sample]).reshape(-1, 1)
+
+            self._dataset.append(_data)
 
     def __len__(self) -> int:
-        return len(self._data)
+        return len(self._dataset)
 
     def __getitem__(self, index: SupportsIndex) -> torch_geometric.data.Data:
-        return self._data[index]
+        return self._dataset[index]
 
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}(size={len(self)})"
+    @property
+    def y(self) -> torch.Tensor:
+        return torch.cat((_data.y for _data in self._dataset), dim=0)
 
-    def __repr__(self) -> str:
-        return str(self)
+    @y.setter
+    def y(self, _value) -> None:
+        raise AttributeError
